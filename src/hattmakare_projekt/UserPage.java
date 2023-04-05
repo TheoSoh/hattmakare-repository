96b6509d59810d9739dfee7129857a558fefb184
filippabostrom <bostromfilippa@gmail.com>
2023-04-05 10:44:02 +0200

    design av UserPage

--- src/hattmakare_projekt/UserPage.java
+++ src/hattmakare_projekt/UserPage.java
@@ -27,33 +27,151 @@ public class UserPage extends javax.swing.JFrame {
     private void initComponents() {
 
         lblHeader = new javax.swing.JLabel();
+        lblHeaderSecond = new javax.swing.JLabel();
+        btnRegisterCustomer = new javax.swing.JButton();
+        btnRegisterOrder = new javax.swing.JButton();
+        btnChangeOrderInfo = new javax.swing.JButton();
+        btnShowOrderInfo = new javax.swing.JButton();
+        btnShowCustomerInfo = new javax.swing.JButton();
+        btnChangeInfoCustomer = new javax.swing.JButton();
+        btnChangeOrderStatus = new javax.swing.JButton();
+        btnShowStatistics = new javax.swing.JButton();
+        btnCreateInvoice = new javax.swing.JButton();
+        btnPrint = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         lblHeader.setFont(new java.awt.Font("Helvetica Neue", 0, 48)); // NOI18N
         lblHeader.setText("Välkommen");
 
+        lblHeaderSecond.setFont(new java.awt.Font("Helvetica Neue", 0, 18)); // NOI18N
+        lblHeaderSecond.setText("Vad vill du göra?");
+
+        btnRegisterCustomer.setText("Registera ny kund");
+        btnRegisterCustomer.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnRegisterCustomerActionPerformed(evt);
+            }
+        });
+
+        btnRegisterOrder.setText("Registrera förfrågan");
+        btnRegisterOrder.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnRegisterOrderActionPerformed(evt);
+            }
+        });
+
+        btnChangeOrderInfo.setText("Ändra i order");
+
+        btnShowOrderInfo.setText("Visa order");
+        btnShowOrderInfo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnShowOrderInfoActionPerformed(evt);
+            }
+        });
+
+        btnShowCustomerInfo.setText("Se information om kund");
+
+        btnChangeInfoCustomer.setText("Ändra information om kund");
+
+        btnChangeOrderStatus.setText("Ändra status på order");
+
+        btnShowStatistics.setText("Se statistik");
+
+        btnCreateInvoice.setText("Skapa faktura");
+
+        btnPrint.setText("Skriv ut frakt och tullsedel");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(97, 97, 97)
-                .addComponent(lblHeader)
-                .addContainerGap(436, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(97, 97, 97)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(lblHeaderSecond, javax.swing.GroupLayout.PREFERRED_SIZE, 181, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(btnRegisterCustomer)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(btnRegisterOrder)
+                                    .addComponent(btnChangeOrderInfo)
+                                    .addComponent(btnChangeOrderStatus))
+                                .addGap(61, 61, 61)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(btnPrint)
+                                    .addComponent(btnCreateInvoice)))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(btnShowCustomerInfo)
+                                    .addComponent(btnChangeInfoCustomer))
+                                .addGap(77, 77, 77)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(btnShowOrderInfo)
+                                    .addComponent(btnShowStatistics)))))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(87, 87, 87)
+                        .addComponent(lblHeader)))
+                .addContainerGap(290, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(65, 65, 65)
                 .addComponent(lblHeader)
-                .addContainerGap(447, Short.MAX_VALUE))
+                .addGap(18, 18, 18)
+                .addComponent(lblHeaderSecond)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnRegisterCustomer)
+                    .addComponent(btnShowOrderInfo))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnShowCustomerInfo)
+                    .addComponent(btnShowStatistics))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(btnChangeInfoCustomer)
+                .addGap(67, 67, 67)
+                .addComponent(btnRegisterOrder)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnChangeOrderInfo)
+                    .addComponent(btnCreateInvoice))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnPrint)
+                    .addComponent(btnChangeOrderStatus))
+                .addContainerGap(129, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void btnRegisterCustomerActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRegisterCustomerActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnRegisterCustomerActionPerformed
+
+    private void btnRegisterOrderActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRegisterOrderActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnRegisterOrderActionPerformed
+
+    private void btnShowOrderInfoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnShowOrderInfoActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnShowOrderInfoActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnChangeInfoCustomer;
+    private javax.swing.JButton btnChangeOrderInfo;
+    private javax.swing.JButton btnChangeOrderStatus;
+    private javax.swing.JButton btnCreateInvoice;
+    private javax.swing.JButton btnPrint;
+    private javax.swing.JButton btnRegisterCustomer;
+    private javax.swing.JButton btnRegisterOrder;
+    private javax.swing.JButton btnShowCustomerInfo;
+    private javax.swing.JButton btnShowOrderInfo;
+    private javax.swing.JButton btnShowStatistics;
     private javax.swing.JLabel lblHeader;
+    private javax.swing.JLabel lblHeaderSecond;
     // End of variables declaration//GEN-END:variables
 }

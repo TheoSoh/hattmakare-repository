3f371038e4db842b12000ec2773273b69f9b7c94
Treo69 <96296042+Treo69@users.noreply.github.com>
2023-04-25 11:20:47 +0200

    Ändringar i sendEmail

--- src/hattmakare_projekt/SendEmail.java
+++ src/hattmakare_projekt/SendEmail.java
@@ -1,9 +1,12 @@
 
 package hattmakare_projekt;
 
+import java.util.ArrayList;
 import oru.inf.InfDB;
 
 import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.mail.Message;
 import javax.mail.MessagingException;
@@ -13,6 +16,7 @@ import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import javax.swing.JOptionPane;
+import oru.inf.InfException;
 /**
  *
  * @author Josef
@@ -24,47 +28,37 @@ public class SendEmail extends javax.swing.JFrame {
     public SendEmail(InfDB idb) {
         initComponents();
         this.idb = idb;
+        fillCustomerBox();
     }
     
-    @SuppressWarnings("unchecked")
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
-    private void initComponents() {
-
-        btnSend = new javax.swing.JButton();
-
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-        btnSend.setText("Skicka");
-        btnSend.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnSendActionPerformed(evt);
-            }
-        });
-
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(129, 129, 129)
-                .addComponent(btnSend)
-                .addContainerGap(199, Short.MAX_VALUE))
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(249, Short.MAX_VALUE)
-                .addComponent(btnSend)
-                .addGap(28, 28, 28))
-        );
-
-        pack();
-    }// </editor-fold>//GEN-END:initComponents
-
-    private void btnSendActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSendActionPerformed
-       
+    private void fillCustomerBox() {
         
-        String to = "josefharknas@hotmail.com";
+         
+        String query = "SELECT Name FROM Customer";
+        
+        ArrayList <String> allaKundNamn;
+        
+        try {
+            allaKundNamn = idb.fetchColumn(query);
+            
+            for(String name:allaKundNamn) {
+                cbCustomer.addItem(name);   
+            }
+            
+        }catch(InfException e) {
+            JOptionPane.showMessageDialog(null, "fel");
+        }
+    }
+    
+    public void sendEmail() {
+        
+        double pricePerHat = 104352;
+        String customerName = cbCustomer.getSelectedItem().toString();
+        
+        
+        String query = "SELECT Email_address FROM Email WHERE Email.Customer "
+                + "IN (SELECT CustomerID FROM Customer WHERE Name = '" + customerName + "')";
+                              
 
         String from = "josef.harknas@gmail.com";
 
@@ -92,6 +86,8 @@ public class SendEmail extends javax.swing.JFrame {
 
         try {
 
+            String to = idb.fetchSingle(query);
+            
             MimeMessage message = new MimeMessage(session);
 
             // Från
@@ -99,23 +95,84 @@ public class SendEmail extends javax.swing.JFrame {
             // Till
             message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
 
-            message.setSubject("Här är ämnesraden");
+            message.setSubject("Orderbekräftelse");
 
-            message.setText("Här skrivs meddelandet");
-
-            System.out.println("sending...");
+            message.setText("Hej, " + "\n" + "Tack för din förfrågan om att beställa en hatt från Otto och Judiths hattar. Hatten du önskar kommer att kosta " 
+                    + pricePerHat + " kr. För att beställa den vänligen svara på detta meddelande." + "\n" + "\n" + "Mvh Otto och Judith");
 
             Transport.send(message);
             JOptionPane.showMessageDialog(null, "Meddelandet har skickats till kunden!");
         } catch (MessagingException mex) {
             mex.printStackTrace();
+        } catch (InfException ex) {
+            Logger.getLogger(SendEmail.class.getName()).log(Level.SEVERE, null, ex);
         }
+        
+    }
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        btnSend = new javax.swing.JButton();
+        cbCustomer = new javax.swing.JComboBox<>();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        btnSend.setText("Skicka");
+        btnSend.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnSendActionPerformed(evt);
+            }
+        });
+
+        cbCustomer.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Välj kund..." }));
+        cbCustomer.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cbCustomerActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(btnSend)
+                .addGap(133, 133, 133))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(38, 38, 38)
+                .addComponent(cbCustomer, javax.swing.GroupLayout.PREFERRED_SIZE, 188, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(350, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(107, 107, 107)
+                .addComponent(cbCustomer, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 238, Short.MAX_VALUE)
+                .addComponent(btnSend)
+                .addGap(24, 24, 24))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btnSendActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSendActionPerformed
+       
+        sendEmail();
     }//GEN-LAST:event_btnSendActionPerformed
+
+    private void cbCustomerActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cbCustomerActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_cbCustomerActionPerformed
 
     
     
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnSend;
+    private javax.swing.JComboBox<String> cbCustomer;
     // End of variables declaration//GEN-END:variables
 }
